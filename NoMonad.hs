2717c1bbfe1e14ec0c4bfeca75779ab3750a9a6a
Alejandro Machado <alemachado02@gmail.com>
2010-06-27 22:35:27 -0430

    Combinado en master

--- NoMonad.hs
+++
@@ -1,7 +0,0 @@
-module Test (module Test) where
-
-data ParserStatus = ParserStatus { token :: String
-                                 , lineNumber :: Int
-                                 , colNumber :: Int
-                                 }
-                                 deriving (Show)
